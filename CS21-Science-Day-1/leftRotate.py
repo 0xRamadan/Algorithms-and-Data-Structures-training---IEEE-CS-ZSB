4713a2cd1264bfe1d2b0dad82f030732a7b35438
Mahmoud Ramadan <Mahmoud Abdallah>
2020-11-21 14:01:04 +0200

    solve first task-CS science day-1

---
+++ CS21-Science-Day-1/leftRotate.py
@@ -0,0 +1,50 @@
+'''
+        CS21-Science-Day-1      
+        name: Mahmoud Abdallah Hassan 
+        task: array left rotation
+        link: https://www.hackerrank.com/challenges/array-left-rotation/problem
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    for i in range(d):
+        arr.append(arr[i])
+    for i in range(d):
+        arr.remove(arr[0])    
+        
+    return arr
+if __name__ == '__main__':
+
+
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+    print(' '.join(map(str, result)))
+    input('press enter to exit...')
+    # fptr.write(' '.join(map(str, result)))
+    # fptr.write('\n')
+
+    # fptr.close()
