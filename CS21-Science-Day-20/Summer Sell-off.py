1cefe7aa241b8867397400422a18db13446eb648
RaymondReddigton <mah64725@gmail.com>
2021-04-03 22:16:12 +0200

    soving today's task

--- CS21-Science-Day-20/Summer Sell-off.py
+++ CS21-Science-Day-20/Summer Sell-off.py
@@ -1,10 +1,18 @@
-if __name__ == "main":
-    numOfDays, sellOffDays = list(map(int, input().split()))
-    totalProductsSold = 0
-    l = []
-    for i in range(numOfDays):
-        numOfProduct, numOfClients = map(int, input().split())
-        totalProductsSold += min(numOfProduct, numOfClients)
-        l.append(min(2*numOfProduct, numOfClients))
-    l.sort()
-    print(totalProductsSold + sum(l[numOfDays - sellOffDays:]))
+#
+# CS21-Science-Day-20
+# Name: Mahmoud Abdallah Hassan
+# GitHub link: https://github.com/RaymondReddigton
+# problem link: https://codeforces.com/contest/810/problem/B
+# Time Complexity: n log(n)
+#
+#
+
+numOfDays, sellOffDays = map(int, input().split())
+totalProductsSold = 0
+l = []
+for i in range(numOfDays):
+    numOfProduct, numOfClients = map(int, input().split())
+    totalProductsSold += min(numOfProduct, numOfClients)
+    l.append(min(2*numOfProduct, numOfClients)-min(numOfProduct,numOfClients))
+l.sort()
+print(totalProductsSold+sum(l[numOfDays-sellOffDays: ]))
