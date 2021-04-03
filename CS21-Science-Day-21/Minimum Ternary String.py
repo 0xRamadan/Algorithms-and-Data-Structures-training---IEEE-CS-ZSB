8d4dfdd77c58a7cc69380aaf2fd5178fa6e4fd52
RaymondReddigton <mah64725@gmail.com>
2021-04-03 22:15:33 +0200

    solvling today's task

---
+++ CS21-Science-Day-21/Minimum Ternary String.py
@@ -0,0 +1,26 @@
+#
+# CS21-Science-Day-21
+# Name: Mahmoud Abdallah Hassan
+# GitHub link: https://github.com/RaymondReddigton
+# problem link: https://codeforces.com/contest/1009/problem/B?f0a28=1
+# Time Complexity: 
+#
+#
+
+tenaryString = input()
+
+numberOfOnes = tenaryString.count('1')
+indexOffirstTwo = tenaryString.find('2')
+
+# last character in tenary String
+if indexOffirstTwo == -1:
+    indexOffirstTwo = len(tenaryString)
+
+temp = tenaryString[:indexOffirstTwo]
+numberOfZeros = temp.count('0')
+
+result = '0'*numberOfOnes + '1'*numberOfOnes + tenaryString[indexOffirstTwo:].replace('1', '')
+print(result)
+
+
+input('press enter to exit...')
